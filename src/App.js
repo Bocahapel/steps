65acf4903e1e124c58e4be17b26ec29bbb234761
Bocahapel <pratamadrian.putra@gmail.com>
2025-05-22 15:04:33 +0700

    updating comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const messages = ["Learn React", "Apply for Jobs", "Profit"];
 
+//set steps using useState
 export default function App() {
   const [step, setSteps] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
@@ -10,22 +11,25 @@ export default function App() {
   //   setIsOpen((current) => !current);
   // }
 
+  //this is event handler for when user press next button
   function handleNext() {
-    if (step < 3) setSteps(step + 1);
+    if (step < 3) setSteps((currentStep) => currentStep + 1);
   }
 
+  //this is event handler for when user press previous button
   function handlePrevious() {
-    if (step > 1) setSteps(step - 1);
+    if (step > 1) setSteps((currentStep) => currentStep - 1);
   }
 
   return (
     <div>
-      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
         &times;
       </button>
       {isOpen && (
         <div className="steps">
           <div className="numbers">
+            {/*when the step is equal, they run the "active" style from css (recolor the button to purple)*/}
             <div className={step >= 1 ? "active" : ""}>1</div>
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
@@ -34,14 +38,19 @@ export default function App() {
             Step {step}: {messages[step - 1]}
           </p>
           <div className="buttons">
+            {/*this is previous button, the button color are hardcoded*/}
             <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              /*when user click the button, js will run the handler function*/
               onClick={handlePrevious}
             >
               Previous
             </button>
+
+            {/*this is next button, the button color are hardcoded*/}
             <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              /*when user click the button, js will run the handler function*/
               onClick={handleNext}
             >
               Next
